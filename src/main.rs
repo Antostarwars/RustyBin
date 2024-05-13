eccb3fe22908b30709b86af1b0ca9849f9b4edbd
Antostarwars <derosaantonio587@gmail.com>
2024-05-13 08:52:46 +0200

    Initialized Backend stuff (Rocket and Templating system)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rocket_dyn_templates::{Template, context};
+
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! {
+        name: "World",
+    })
 }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index])
+    .attach(Template::fairing())
+}
+
